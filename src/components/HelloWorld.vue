0eb8cf2582ef650ade0db042663d366d1c79d4eb
Oscar Andrés Granada Baquero <oscar.andres.granada@gmail.com>
2021-10-20 18:34:02 -0500

    First commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <img :src="image" alt="">
+</template>
+<script>
+import axios from 'axios';
+
+export default {
+  props: {
+    msg: {
+      type: String
+    }
+  },
+  data: () => ({
+    image: null
+  }),
+  async mounted() {
+    const res = await axios.get('http://localhost:5000/api/images');
+    if (res.status === 200) {
+      this.image = res.data.image;
+    }
+  }
+}
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
